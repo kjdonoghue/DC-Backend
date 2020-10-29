4ff099615c03fac90562b0e890027fc0965c31fc
Katy Donoghue <kathrynjdonoghue@gmail.com>
2020-10-29 09:54:55 -0400

    moved function authenticate from app.js to own file

--- trip-tracking/app.js
+++ trip-tracking/app.js
@@ -3,8 +3,6 @@ const app = express()
 const mustacheExpress = require('mustache-express')
 const { v4: uuidv4 } = require('uuid');
 const tripsRouter = require("./routes/trips") 
-// const registrationRouter = require("./routes/registration")
-// const loginRouter = require("./routes/login")
 const indexRouter = require("./routes/index")
 const session = require("express-session")
 const path = require("path")
@@ -13,8 +11,7 @@ const bcrypt = require('bcryptjs')
 const pgp = require("pg-promise")()
 const connectionString = "postgres://localhost:5432/tripsdatabase"
 const db = pgp(connectionString)
-
-// global.users = []
+const authenticate = require('./authenticate')
 
 app.use(express.urlencoded())
 app.use("/css", express.static("css"))
@@ -27,28 +24,13 @@ app.use(session({
   }))
 
 
-
-// app.use("/login", loginRouter)
-app.use("/trips", tripsRouter)
-//app.use("/trips", authenticate, tripsRouter)
+app.use("/trips", authenticate, tripsRouter)
 app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"))
 app.set("views", VIEWS_PATH)
 app.set("view engine", "mustache")
 app.use("/", indexRouter)
 
-function authenticate(req, res, next) {
-    if (req.session) {
-        if (req.session.username) {
-            next()
-        } else {
-            res.redirect("/login")
-        }
 
-    } else {
-        res.redirect("/login")
-    }
-
-}
 
 app.listen(3000, () => {
     console.log("the server is running")
